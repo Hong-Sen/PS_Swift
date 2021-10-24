8e4146d9aad06999f4ee14b5ae0f473f86bd1c70
Hong-Sen <20181053@sungshin.ac.kr>
2021-10-24 14:39:43 +0900

    조이스틱(수정)

--- 조이스틱/조이스틱/main.swift
+++ 조이스틱/조이스틱/main.swift
@@ -2,21 +2,22 @@ import Foundation
 
 func solution(_ name:String) -> Int {
     var cnt = 0
-    let nameArr = name.map({$0}) // string to character array
+    let name = name.map({$0}) // string to character array
     // 커서 상관없이 알파벳이 바뀌는 횟수 구하기
     for i in 0..<name.count {
-        if nameArr[i] != "A" {
-            let n1 = nameArr[i].asciiValue! - 65
-            let n2 = 91 - nameArr[i].asciiValue!
+        if name[i] != "A" {
+            let n1 = name[i].asciiValue! - 65
+            let n2 = 91 - name[i].asciiValue!
             cnt += min(Int(n1), Int(n2))
         }
     }
+
     // 커서 횟수 구하기
     var move = name.count-1
     for i in 0..<name.count {
-        if nameArr[i] != "A" {
+        if name[i] != "A" {
             var next = i+1
-            while next<name.count && nameArr[next] == "A" {
+            while next<name.count && name[next] == "A" {
                 next += 1
             }
             let tmp = 2*i + name.count - next
@@ -24,7 +25,6 @@ func solution(_ name:String) -> Int {
             move = min(tmp, move)
         }
     }
-    print("\(cnt) \(move)")
     return cnt+move
 }
 
