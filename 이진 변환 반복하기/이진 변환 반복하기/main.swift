c61d706b1ec945180e14782b56136a3fced6afaf
Hong-Sen <20181053@sungshin.ac.kr>
2022-08-23 19:30:01 +0900

    [fix] 이진변환 반복하기
    String(n, radix:2)를 사용하여 2진수로 변환하기

--- 이진 변환 반복하기/이진 변환 반복하기/main.swift
+++ 이진 변환 반복하기/이진 변환 반복하기/main.swift
@@ -1,26 +1,15 @@
 import Foundation
 
-func toBinary(_ n: Int) -> String {
-    var r = n;
-    var s = "";
-    while r != 0 {
-        s = String(r % 2) + s
-        r /= 2
-    }
-    return s
-}
-
 func solution(_ s:String) -> [Int] {
     var changeCnt = 0
     var removedZero = 0
     var str = s
     
-    var i = 0
     while str != "1" {
         removedZero += str.filter{($0 == "0")}.count
         str = str.components(separatedBy: "0").joined()
         changeCnt += 1
-        str = toBinary(str.count)
+        str = String(str.count, radix:2)
     }
     return [changeCnt, removedZero]
 }
